7d99dc99560ebaa9ad31e8f3eceab45c7aa4cdfe
David <david.owuor@student.moringaschool.com>
2023-03-27 19:03:48 +0300

    signup functionalities

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Signup from './components/Signup';
 import Login from './components/Login';
+//import { ToastContainer, toast } from 'react-toastify';
+//import {Routes,Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+        <Signup/>
         <Login/>
     </div>
   );
--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -1,21 +1,63 @@
+//import React from "react";
+//import { useNavigate } from 'react-router-dom';
+//import { toast } from "react-toastify";
+
+
 function Signup(){
 
+   // const navigate = useNavigate();
+
+
+    // function to handle new registrations of applicants
+    // function to post applicants to database
+
+    function handleSignup(e){
+        
+        e.preventDefault()
+
+        const newApplicant={
+            firstname : e.target.firstname.value,
+            lastname : e.target.lastname.value,
+            username : e.target.username.value,
+            email : e.target.email.value,
+            dob : e.target.dob.value,
+        }
+
+        //Post new users
+
+        fetch('http://localhost:8001/applicants', {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              "Accept": "application/json",
+            },
+            body: JSON.stringify(newApplicant),
+          })
+        .then(res=>res.json())
+        .then(data=>{
+            console.log(data);
+
+           // toast.success('Succesfully Registered')
+            //navigate('/login')
+        })
+
+    }
 
     return(
         <div>
             <div>
-                <form>
+                <form onSubmit={handleSignup}>
                     <h1>SignUp</h1>
-                        <input type='text' placeholder="first name"/>
-                        <input type='text' placeholder="last name"/>
-                        <input type='text' placeholder="username"/>
-                        <input type='email' placeholder="email"/>
-                        <input type='date' placeholder="DOB"/>
+                        <input name="firstname" type='text' placeholder="first name"/>
+                        <input name="lastname" type='text' placeholder="last name"/>
+                        <input name="username" type='text' placeholder="username"/>
+                        <input name="email" type='email' placeholder="email"/>
+                        <input name="dob" type='date' placeholder="DOB"/>
                         <input type='submit'/>
 
                 </form>
                     {/* button for switching accounts */}
-                    <button>already have an account? login</button>
+                    <p>Already have an account? <a href="!#">Login</a></p>
             </div>
         </div>
     )
