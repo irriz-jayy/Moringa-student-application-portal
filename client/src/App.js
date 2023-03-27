040a63f5bdcaaf2271c5c2f78cae8d6c0e9e1806
David <david.owuor@student.moringaschool.com>
2023-03-27 14:53:29 +0300

    login and signup first commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Signup from './components/Signup';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
-    
+        <Login/>
     </div>
   );
 }
