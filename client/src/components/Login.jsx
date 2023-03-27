6414d461b3590f94e1b6cc88fa93be9cfe2e9860
David <david.owuor@student.moringaschool.com>
2023-03-27 15:26:45 +0300

    added forms of signup and login

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,13 +1,17 @@
 function Login(){
 
-    
+
     return (
         <div>
             <div>
-                <h1>LOGIN</h1>
-                    <input type='text' placeholder="username"/>
-                    <input type='password' placeholder="password"/>
-                    <input type='submit'/>
+                <form>
+                    <h1>LOGIN</h1>
+                        <input type='text' placeholder="username"/>
+                        <input type='password' placeholder="password"/>
+                        <input type='submit'/>
+                </form>
+                       {/* button for switching accounts */}
+                       <button>do not have an account? Signup</button>
             </div>
         </div>
     )
--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -1,14 +1,24 @@
 function Signup(){
+
+
+
+
     return(
         <div>
             <div>
-                <h1>SignUp</h1>
-                    <input type='text' placeholder="first name"/>
-                    <input type='text' placeholder="last name"/>
-                    <input type='text' placeholder="username"/>
-                    <input type='email' placeholder="email"/>
-                    <input type='date' placeholder="DOB"/>
-                    <input type='submit'/>
+                <form>
+                    <h1>SignUp</h1>
+                        <input type='text' placeholder="first name"/>
+                        <input type='text' placeholder="last name"/>
+                        <input type='text' placeholder="username"/>
+                        <input type='email' placeholder="email"/>
+                        <input type='date' placeholder="DOB"/>
+                        <input type='submit'/>
+
+                </form>
+                    {/* button for switching accounts */}
+                    <button>already have an account? login</button>
+
             </div>
         </div>
     )
