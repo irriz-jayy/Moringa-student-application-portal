5684cbb0ffcde46b722c67abcd1ac53629ed9be7
David <david.owuor@student.moringaschool.com>
2023-03-27 14:53:29 +0300

    login and signup first commit

---
+++ client/src/components/Login.jsx
@@ -0,0 +1,16 @@
+function Login(){
+
+    
+    return (
+        <div>
+            <div>
+                <h1>LOGIN</h1>
+                    <input type='text' placeholder="username"/>
+                    <input type='password' placeholder="password"/>
+                    <input type='submit'/>
+            </div>
+        </div>
+    )
+}
+
+export default Login;
---
+++ client/src/components/Signup.jsx
@@ -0,0 +1,17 @@
+function Signup(){
+    return(
+        <div>
+            <div>
+                <h1>SignUp</h1>
+                    <input type='text' placeholder="first name"/>
+                    <input type='text' placeholder="last name"/>
+                    <input type='text' placeholder="username"/>
+                    <input type='email' placeholder="email"/>
+                    <input type='date' placeholder="DOB"/>
+                    <input type='submit'/>
+            </div>
+        </div>
+    )
+}
+
+export default Signup;
