a030b7c04f0ffb517c8ed46824a7568c5571be1c
Joanna <astronautnemo@gmail.com>
2023-03-27 17:15:28 +0300

    added the LandingPage HTML

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import Signup from './components/Signup';
 import Login from './components/Login';
+import LandingPage from './containers/LandingPage'
 
 function App() {
   return (
     <div className="App">
+        <LandingPage/>
         <Login/>
     </div>
   );
