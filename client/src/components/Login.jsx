69b10cf8580ab9d6f9c4d9f11ca91d426539ab52
Joanna <astronautnemo@gmail.com>
2023-03-28 21:16:00 +0300

    added styling to Login component

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,17 +1,36 @@
+import "./Form.css"
+
 function Login(){
 
 
     return (
-        <div>
-            <div>
+        <div className="page container-flex">
+            <div className="form">
+                
+                <h1 className="form-title">Login</h1>
+
                 <form>
-                    <h1>LOGIN</h1>
-                        <input type='text' placeholder="username"/>
-                        <input type='password' placeholder="password"/>
-                        <input type='submit'/>
+
+                    <div className="mb-3">
+                        <label className="form-label">Username</label>
+                        <input className="form-control" name="username" type='text' placeholder="Username"/>
+                    </div>
+
+                    <div className="mb-3">
+                        <label className="form-label">Email</label>
+                        <input className="form-control" name="email" type='email' placeholder="Email"/>
+                    </div>
+
+                    <div className="form-submit">
+                        <button type="submit" className="btn">Log In</button>
+                    </div>
+
                 </form>
-                       {/* button for switching accounts */}
-                       <button>do not have an account? Signup</button>
+
+                {/* Switch to Signing Up */}
+                <p className="container">
+                    Don't have an account?<a href="/signup">Sign up</a>
+                </p>
             </div>
         </div>
     )
