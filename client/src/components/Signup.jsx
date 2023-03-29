89f223f0ccbd24faa614cd35d5d4df6f3d14a92f
David <david.owuor@student.moringaschool.com>
2023-03-29 17:13:22 +0300

    signup post functionalities

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -1,85 +1,107 @@
 import "./Form.css"
-//import React from "react";
+import React, {useState} from "react";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 
 function Signup() {
   const navigate = useNavigate();
+  const [signupFormData, setSignupFormData] = useState({
+    first_name: '',
+      last_name: '',
+      username: '',
+      email:'',
+      date_of_birth: ''
+  })
 
   // function to handle new registrations of applicants
   // function to post applicants to database
+  function handleSignupChange(e){
+      const name = e.target.name
+      const value = e.target.value
+      setSignupFormData({
+        ...signupFormData,[name] :value
+      })
+  }
+
+    console.log(signupFormData)
 
   function handleSignup(e) {
     e.preventDefault();
 
-    const newApplicant = {
-      firstname: e.target.firstname.value,
-      lastname: e.target.lastname.value,
-      username: e.target.username.value,
-      email: e.target.email.value,
-      dob: e.target.dob.value,
-    };
+
+
+    // const newApplicant = {
+    //   first_name: e.target.first_name.value,
+    //   last_name: e.target.last_name.value,
+    //   username: e.target.username.value,
+    //   email: e.target.email.value,
+    //   date_of_birth: e.date_of_birth.value
+    // };
 
     //Post new users
 
-    fetch("http://localhost:8001/applicants", {
+    //console.log(newApplicant);
+
+    fetch("/applicants", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
         Accept: "application/json",
       },
-      body: JSON.stringify(newApplicant),
+      body: JSON.stringify(signupFormData),
     })
       .then((res) => res.json())
       .then((data) => {
+        console.log(data)
         //Display succesfull response
         toast.success("Succesfully Registered");
         //navigates to login page
         //will redirect to login page
         navigate("/login");
       });
+      e.target.reset()
   }
 
   return (
     <div className="page container-flex">
       <div className="form">
-        
+
         <h1 className="form-title">Signup</h1>
 
         <form onSubmit={handleSignup}>
           <div className="mb-3">
             <label className="form-label">First Name</label>
-            <input className="form-control" name="firstname" type="text" placeholder="First Name" />
+            <input className="form-control" value={signupFormData.first_name} onChange={handleSignupChange} name="first_name" type="text" placeholder="First Name" />
           </div>
 
           <div className="mb-3">
             <label className="form-label">Last Name</label>
-            <input className="form-control" name="lastname" type="text" placeholder="Last Name" />
+            <input className="form-control" value={signupFormData.last_name} onChange={handleSignupChange} name="last_name" type="text" placeholder="Last Name" />
           </div>
 
           <div className="mb-3">
             <label className="form-label">Username</label>
-            <input className="form-control" name="username" type="text" placeholder="Username" />
+            <input className="form-control" value={signupFormData.username} onChange={handleSignupChange} name="username" type="text" placeholder="Username" />
           </div>
-          
+
           <div className="mb-3">
             <label className="form-label">Email</label>
-            <input className="form-control" name="email" type="email" placeholder="Email" />
+            <input className="form-control" value={signupFormData.email} onChange={handleSignupChange} name="email" type="email" placeholder="Email" />
           </div>
 
           <div className="mb-3">
             <label className="form-label">Date of Birth</label>
-            <input className="form-control" name="dob" type="date" placeholder="Date of Birth" />
+            <input className="form-control" value={signupFormData.date_of_birth} onChange={handleSignupChange} name="date_of_birth" type="date" placeholder="Date of Birth" />
           </div>
 
           <div className="form-submit">
             <button type="submit" className="btn">Sign Up</button>
           </div>
-        
-          
+
+
 
         </form>
-        
+
         {/* Switch to Logging In */}
         <p className="container">
             Already have an account?<a href="/login">Log in</a>
