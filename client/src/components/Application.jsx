10597bbe26fcb20da58fea8ba4ea5cc02699523d
Joanna <astronautnemo@gmail.com>
2023-04-02 11:03:23 +0300

    added LoadingScreen component

--- client/src/components/Application.jsx
+++ client/src/components/Application.jsx
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from "react";
 import { Link, useParams } from "react-router-dom";
+import LoadingScreen from "./LoadingScreen";
 
 
 function Application({currentUser, navigate}){
@@ -9,24 +10,18 @@ function Application({currentUser, navigate}){
     const {id} = useParams()
 
 
-      // Fetch single course
-      useEffect(() => {
-        fetch(`/courses/${id}`)
-        .then(res => {
-            if(res.ok){
-                res.json().then(course => setCourse(course))
-            }
-        })
+    // Fetch single course
+    useEffect(() => {
+    fetch(`/courses/${id}`)
+    .then(res => {
+        if(res.ok){
+            res.json().then(course => setCourse(course))
+        }
+    })
 
-        window.scrollTo(0, 0)
+    window.scrollTo(0, 0)
     }, [])
-
-    //   console.log(currentUser);
-
-   // const courseID = course.id
-
-   // console.log(courseID));
-
+    
     function handleApplicationSubmit(e){
         e.preventDefault()
 
@@ -38,9 +33,7 @@ function Application({currentUser, navigate}){
             documents: e.target.documents.value,
             status: "Submitted"
         }
-        console.log(newApplication);
-
-
+        
         //post request for new applications
 
         fetch('/applications',{
@@ -57,51 +50,52 @@ function Application({currentUser, navigate}){
         })
     }
 
-    return (
-        <div className="page container-flex">
-            <div className="form">
-                <h1 className="form-title">New Application</h1>
-
-                <form onSubmit={handleApplicationSubmit}>
-
-                    <div className="mb-3">
-                        <label className="form-label">Course Name</label>
-                        <input className="form-control" value={course.title} type="text" disabled/>
-                    </div>
-
-                    <div className="mb-3">
-                        <label className="form-label">Applicant's Name</label>
-                        <input className="form-control" value={`${currentUser.first_name} ${currentUser.last_name}`} type="text" disabled/>
-                    </div>
-
-                    <div className="mb-3">
-                        <label className="form-label">Status</label>
-                        <input className="form-control" value={'Applying'} name="status" type="text" disabled/>
-                    </div>
-
-                    <div className="mb-3">
-                        <label className="form-label">DOB</label>
-                        <input className="form-control" value={currentUser.date_of_birth} type="date" disabled/>
-                    </div>
-
-                    <div className="mb-3">
-                        <label className="form-label">Documents</label>
-                        <br/>
-                        <p className="form-notes">Please list your documents separated by a comma</p>
-                        <input className="form-control" name="documents" type="text"/>
-                    </div>
-
-                    <div className="form-submit">
-                         <button type="submit" className="btn">Submit</button>
-                    </div>
-
-                </form>
+    if(course.title === undefined){
+        return <LoadingScreen />
+    }else{
+        return (
+            <div className="page container-flex">
+                <div className="form">
+                    <h1 className="form-title">New Application</h1>
+    
+                    <form onSubmit={handleApplicationSubmit}>
+    
+                        <div className="mb-3">
+                            <label className="form-label">Course Name</label>
+                            <input className="form-control" value={course.title} type="text" disabled/>
+                        </div>
+    
+                        <div className="mb-3">
+                            <label className="form-label">Applicant's Name</label>
+                            <input className="form-control" value={`${currentUser.first_name} ${currentUser.last_name}`} type="text" disabled/>
+                        </div>
+    
+                        <div className="mb-3">
+                            <label className="form-label">Status</label>
+                            <input className="form-control" value={'Applying'} name="status" type="text" disabled/>
+                        </div>
+    
+                        <div className="mb-3">
+                            <label className="form-label">DOB</label>
+                            <input className="form-control" value={currentUser.date_of_birth} type="date" disabled/>
+                        </div>
+    
+                        <div className="mb-3">
+                            <label className="form-label">Documents</label>
+                            <br/>
+                            <p className="form-notes">Please list your documents separated by a comma</p>
+                            <input className="form-control" name="documents" type="text"/>
+                        </div>
+    
+                        <div className="form-submit">
+                             <button type="submit" className="btn">Submit</button>
+                        </div>
+    
+                    </form>
+                </div>
             </div>
-        </div>
-
-
-
-
-    )
+        )
+    }
+    
 }
 export default Application;
