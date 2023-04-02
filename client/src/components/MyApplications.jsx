200b9c27b2202018b618e9dda916fecc5e7605d8
Joanna <astronautnemo@gmail.com>
2023-04-02 10:59:16 +0300

    added LoadingScreen component

--- client/src/components/MyApplications.jsx
+++ client/src/components/MyApplications.jsx
@@ -3,6 +3,7 @@ import { VscCalendar } from "react-icons/vsc";
 import { Link } from "react-router-dom";
 import React, {useState, useEffect} from "react";
 import MyApplicationsCard from "./MyApplicationsCard";
+import LoadingScreen from "./LoadingScreen";
 
 
 function MyApplications({currentUser}){
@@ -27,8 +28,6 @@ function MyApplications({currentUser}){
       return app.applicant.id === currentUser.id
     })
 
-    //console.log(filteredApplications);
-
 
     //function to MAP applications by logged in user
 
@@ -36,22 +35,24 @@ function MyApplications({currentUser}){
       return  <MyApplicationsCard application={application} key={id}/>
     })
 
-
-
-
-    return(
-      <div className="page card-page">
-        <div className="page-header">
-          <h1>My Applications</h1>
+    if(mapped.length === 0){
+      return <LoadingScreen />
+    }else{
+      return(
+        <div className="page card-page">
+          <div className="page-header">
+            <h1>My Applications</h1>
+          </div>
+  
+          <div className="card-grid">
+            <div className="row row-cols-sm-1 row-cols-md-1 row-cols-lg-2 g-5">
+              {mapped}
+            </div> 
+          </div>
+  
         </div>
+      )
+    }
 
-        <div className="card-grid">
-          <div className="row row-cols-sm-1 row-cols-md-1 row-cols-lg-2 g-5">
-            {mapped}
-          </div> 
-        </div>
-
-      </div>
-    )
 }
     export default MyApplications
